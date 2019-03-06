81db58f581b9d0a30ce9357f1719a418d4391ed5
teran <petersonteran@gmail.com>
2019-03-05 22:03:52 -0700

    Working on load service

--- src/com/teranpeterson/server/service/LoadService.java
+++ src/com/teranpeterson/server/service/LoadService.java
@@ -1,7 +1,11 @@
 package com.teranpeterson.server.service;
 
+import com.teranpeterson.server.dao.DAOException;
+import com.teranpeterson.server.dao.Database;
 import com.teranpeterson.server.request.LoadRequest;
 import com.teranpeterson.server.result.LoadResult;
+
+import java.sql.Connection;
 
 /**
  * Loads the provided user, person and event data into the database.
@@ -24,6 +28,17 @@ public class LoadService extends Service {
      * @return Information about whether it was successful or not
      */
     public LoadResult load(LoadRequest request) {
+        Database db = new Database();
+        try {
+            Connection conn = db.openConnection();
+        } catch (DAOException e) {
+            try {
+                db.closeConnection(false);
+                return new LoadResult(e.getMessage());
+            } catch (DAOException d) {
+                return new LoadResult(d.getMessage());
+            }
+        }
         return null;
     }
 }
