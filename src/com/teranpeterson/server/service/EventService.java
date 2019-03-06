eb222fee6a38cfd5d1365cbf6c1825252960fb3e
teran <petersonteran@gmail.com>
2019-03-05 22:46:54 -0700

    Added event service functionality. Ready for testing

--- src/com/teranpeterson/server/service/EventService.java
+++ src/com/teranpeterson/server/service/EventService.java
@@ -1,13 +1,21 @@
 package com.teranpeterson.server.service;
 
+import com.teranpeterson.server.dao.AuthTokenDAO;
+import com.teranpeterson.server.dao.DAOException;
+import com.teranpeterson.server.dao.Database;
+import com.teranpeterson.server.dao.EventDAO;
+import com.teranpeterson.server.model.Event;
 import com.teranpeterson.server.request.EventRequest;
 import com.teranpeterson.server.result.EventResult;
+
+import java.sql.Connection;
+import java.util.List;
 
 /**
  * Returns the single Event object with the specified ID or returns ALL events for ALL family members of the current user.
  *
  * @author Teran Peterson
- * @version v0.0.1
+ * @version v0.1.1
  */
 public class EventService extends Service {
     /**
@@ -20,10 +28,35 @@ public class EventService extends Service {
     /**
      * If the request object contains an eventID, that event is returned. Otherwise, ALL events for ALL family members related
      * to the current user (determined by the auth token) are returned.
+     *
      * @param request Information about the event(s) to return
      * @return Information about the events
      */
     public EventResult event(EventRequest request) {
-        return null;
+        Database db = new Database();
+        try {
+            db.createTables();
+            Connection conn = db.openConnection();
+            AuthTokenDAO aDAO = new AuthTokenDAO(conn);
+            String username = aDAO.validate(request.getAuthToken());
+            if (username == null) return new EventResult("ERROR: Invalid auth token");
+            EventDAO eDAO = new EventDAO(conn);
+            if (request.getEventID().equals("ALL")) {
+                List<Event> list = eDAO.personEvents(username);
+                db.closeConnection(true);
+                return new EventResult(list);
+            } else {
+                Event event = eDAO.find(request.getEventID());
+                db.closeConnection(true);
+                return new EventResult(event);
+            }
+        } catch (DAOException e) {
+            try {
+                db.closeConnection(false);
+                return new EventResult(e.getMessage());
+            } catch (DAOException d) {
+                return new EventResult(d.getMessage());
+            }
+        }
     }
 }
